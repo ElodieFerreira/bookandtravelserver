0738792badbf474eed76a6f792af3833689ca537
MelVingt <jesiolowski.melvin@gmail.com>
2020-10-08 16:16:37 +0200

    API utilisateur

--- app/user/user.controller.js
+++ app/user/user.controller.js
@@ -5,10 +5,7 @@ const express = require('express');
 const router = express.Router();
 const userService = require('./user.service');
 
-test = (req, res) =>{
-    console.log("hello");
-    res.send("coucou");
-}
+
 findById = (req, res) => {
     userService.findById(req.params.id, (err, data) => {
         if (err) {
@@ -25,7 +22,61 @@ findById = (req, res) => {
     });
 };
 
+post = (req, res) => {
+    userService.post(req.body.Nom, req.body.Prenom, req.body.Mail, req.body.Telephone, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found user with id ${req.params.customerId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving user with id " + req.params.customerId
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+put = (req, res) => {
+    userService.put(req.params.id, req.body.Nom, req.body.Prenom, req.body.Mail, req.body.Telephone, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found user with id ${req.params.customerId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving user with id " + req.params.customerId
+                });
+            }
+        } else res.send(data);
+    });
+};
+
+deleteUser = (req, res) => {
+    userService.deleteUser(req.params.id, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `Not found user with id ${req.params.customerId}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving user with id " + req.params.customerId
+                });
+            }
+        } else res.send(data);
+    });
+};
+
 router.get('/:id',findById);
 router.get('/',test);
+
+router.post('/', post); 
+
+router.put('/:id',put);
+
+router.delete('/:id',deleteUser);
 
 module.exports = router;
--- app/user/user.service.js
+++ app/user/user.service.js
@@ -22,4 +22,61 @@ userService.findById = (userId, result) => {
     });
 };
 
+userService.post = (nom, prenom, mail, telephone, result) => {
+    pool.query(`INSERT INTO utilisateur(Nom, Prenom, Mail, Telephone) VALUES ('${nom}', '${prenom}', '${mail}', '${telephone}')`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found user ", res[0]);
+            result(null, res[0]);
+            return;
+        }
+
+        // not found Customer with the id
+        result({ kind: "not_found" }, null);
+    });
+};
+
+userService.put = (id, nom, prenom, mail, telephone, result) => {
+    pool.query(`UPDATE utilisateur SET Nom = '${nom}', Prenom = '${prenom}' , Mail= '${mail}', Telephone= '${telephone}' WHERE ID = ${id}`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found user ", res);
+            result(null, res);
+            return;
+        }
+
+        // not found Customer with the id
+        result({ kind: "not_found" }, null);
+    });
+};
+
+userService.deleteUser = (id, result) => {
+    pool.query(`DELETE FROM utilisateur WHERE ID = ${id}`, (err, res) => {
+        if (err) {
+            console.log("error: ", err);
+            result(err, null);
+            return;
+        }
+
+        if (res.length) {
+            console.log("found user ", res);
+            result(null, res);
+            return;
+        }
+
+        // not found Customer with the id
+        result({ kind: "not_found" }, null);
+    });
+};
+
 module.exports = userService;
